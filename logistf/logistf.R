67795b56a2e5e979378ae22378b7a05f0ad40b60
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-06-29 23:56:24 +0700

    Use logistf With (In) R Software

--- logistf/logistf.R
+++ logistf/logistf.R
@@ -8,6 +8,7 @@ logistf = read.csv("https://raw.githubusercontent.com/timbulwidodostp/logistf/ma
 # Estimation Firth's (Firth) Bias-Reduced Logistic Regression Use logistf With (In) R Software
 logistf <- logistf(formula = case ~ age + oc + vic + vicl + vis + dia, data = logistf)
 summary(logistf)
+
 # Firth's (Firth) Bias-Reduced Logistic Regression Use logistf With (In) R Software
 # Olah Data Semarang
 # WhatsApp : +6285227746673
